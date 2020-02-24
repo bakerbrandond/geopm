122f35c61be88db46fc4d08f1a4ff26ed6f38424
Brandon Baker <brandon.baker@intel.com>
2020-02-24 10:53:23 -0800

    Move geopmread/write and other utils to geopmpy.launcher
    Change-Id: I24a503bf936c02759949306ec3ee867488a61210
    Signed-off-by: Brandon Baker <brandon.baker@intel.com>

--- scripts/geopmpy/launcher.py
+++ scripts/geopmpy/launcher.py
@@ -65,6 +65,104 @@ from collections import OrderedDict
 from geopmpy import __version__
 
 
+def detect_launcher():
+    """
+    Heuristic to determine the resource manager used on the system.
+    Returns name of resource manager or launcher, otherwise a
+    LookupError is raised.
+    """
+    # Try the environment
+    result = os.environ.get('GEOPM_LAUNCHER', None)
+    if not result:
+        # Check for known host names
+        slurm_hosts = ['mr-fusion', 'mcfly']
+        alps_hosts = ['theta']
+        hostname = socket.gethostname()
+        if any(hostname.startswith(word) for word in slurm_hosts):
+            result = 'srun'
+        elif any(hostname.startswith(word) for word in alps_hosts):
+            result = 'aprun'
+    if not result:
+        try:
+            exec_str = 'srun --version'
+            subprocess.check_call(exec_str, stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE, shell=True)
+            result = 'srun'
+        except subprocess.CalledProcessError:
+            pass
+    if not result:
+        try:
+            exec_str = 'aprun --version'
+            subprocess.check_call(exec_str, stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE, shell=True)
+            result = 'aprun'
+        except subprocess.CalledProcessError:
+            pass
+    if not result:
+        raise LookupError('Unable to determine resource manager')
+    return result
+
+def geopmwrite(write_str):
+    test_exec = "dummy -- geopmwrite " + write_str
+    stdout = io.StringIO()
+    stderr = io.StringIO()
+    try:
+        allocation_node_test(test_exec, stdout, stderr)
+    except subprocess.CalledProcessError as err:
+        sys.stderr.write(stderr.getvalue())
+        raise err
+    output = stdout.getvalue().splitlines()
+    last_line = None
+    if len(output) > 0:
+        last_line = output[-1]
+    return last_line
+
+def geopmread(read_str):
+    test_exec = "dummy -- geopmread " + read_str
+    stdout = io.StringIO()
+    stderr = io.StringIO()
+    try:
+        allocation_node_test(test_exec, stdout, stderr)
+    except subprocess.CalledProcessError as err:
+        sys.stderr.write(stderr.getvalue())
+        raise err
+    last_line = stdout.getvalue().splitlines()[-1]
+
+    if last_line.startswith('0x'):
+        return int(last_line)
+    else:
+        try:
+            return float(last_line)
+        except ValueError:
+            return last_line
+
+def allocation_node_test(test_exec, stdout, stderr):
+    argv = shlex.split(test_exec)
+    launcher = detect_launcher()
+    argv.insert(1, launcher)
+    if launcher == 'aprun':
+        argv.insert(2, '-q') # Use quiet flag with aprun to suppress end of job info string
+    argv.insert(2, '--geopm-ctl-disable')
+    launcher = Factory().create(argv, num_rank=1, num_node=1,
+                                                 job_name="geopm_allocation_test", quiet=True)
+    launcher.run(stdout, stderr)
+
+def get_platform():
+    test_exec = "dummy -- cat /proc/cpuinfo"
+    ostream = io.StringIO()
+    dev_null = open('/dev/null', 'w')
+    allocation_node_test(test_exec, ostream, dev_null)
+    dev_null.close()
+    output = ostream.getvalue()
+
+    for line in output.splitlines():
+        if line.startswith('cpu family\t:'):
+            fam = int(line.split(':')[1])
+        if line.startswith('model\t\t:'):
+            mod = int(line.split(':')[1])
+    return fam, mod
+
+
 class Factory(object):
     def __init__(self):
         self._launcher_dict = OrderedDict([('srun', SrunLauncher),
