450778f9c4eaa91571abeba5e200bbbb6f38f0b3
Brandon Baker <brandon.baker@intel.com>
2020-02-26 18:07:20 -0800

    Update TimedScalingRegion to match new base class interface
    Change-Id: I998692dedf24ebf2108a02c7768d2ffa734daa16
    Signed-off-by: Brandon Baker <brandon.baker@intel.com>

--- src/TimedScalingModelRegion.cpp
+++ src/TimedScalingModelRegion.cpp
@@ -36,12 +36,13 @@
 
 namespace geopm
 {
-    TimedScalingModelRegion::TimedScalingModelRegion(double big_o_in,
+    TimedScalingModelRegion::TimedScalingModelRegion(const std::string &name,
+                                                     double big_o_in,
                                                      int verbosity,
                                                      bool do_imbalance,
                                                      bool do_progress,
                                                      bool do_unmarked)
-        : SpinModelRegion(big_o_in, verbosity, do_imbalance, do_progress, do_unmarked)
+        : SpinModelRegion(name, big_o_in, verbosity, do_imbalance, do_progress, do_unmarked)
         , m_scaling_model(std::make_shared<ScalingModelRegion>(1, 0, false, false, true))
     {
 
